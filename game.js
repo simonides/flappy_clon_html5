03e0b3ca6826ce8fceb5de3533dda50577723349
jakobmaier <jakobmaier42@gmail.com>
2016-03-09 19:39:44 +0100

    Implemented pipes

--- game.js
+++ game.js
@@ -5,6 +5,10 @@ function GameRunner(_context){
     var lastTimestamp = 0;
 
     var background;
+	var holeHeight = 100;
+	var pipeSpacing = 250;
+	var pipes = [];
+
 
     function construct() {
     	background = new Background(context);
@@ -14,6 +18,12 @@ function GameRunner(_context){
 
 
     self.start = function() {
+    	var width = context.getViewPort().width();
+    	var pipeCount = width / pipeSpacing;	// I ingore the pipe width
+    	for(var i = 0; i < pipeCount; ++i) {
+    		addPipes(width + i * pipeSpacing);
+		}
+
     	scheduler();
     }
 
@@ -28,37 +38,61 @@ function GameRunner(_context){
     	gameLoop(elapsedTime);
 
 
-    	addPipes(100, 500);
     	// console.log("animate", elapsedTime);
 	}
 
-	var distance = 0;
-	// var Pipe = 0;
-	var pipes = [];
+
+
+
+
 
 	function gameLoop(elapsedTime) {
 		
-		distance += elapsedTime;
-
+		movePipes(elapsedTime/2);
 		//
 	}
 
 
-	function addPipes(posX, holeY) {
+	function movePipes(distance) {
+		if(pipes.length == 0) {
+			return;
+		}
+		var pipeWidth = pipes[0][0].getSize().x;
+		var spliceCount = 0;
+		for(var i=0; i<pipes.length; ++i) {
+			pipes[i][0].translate({x: distance, y: 0});
+			var newPos = pipes[i][1].translate({x: distance, y: 0});
+			if(newPos.x < -pipeWidth){
+				++spliceCount;
+			}
+		}
+		var replace = pipes.splice(0, spliceCount);
+		for(var i=0; i<replace.length; ++i) {
+			replace[i][0].destroy();
+			replace[i][1].destroy();
+			newPos.x += pipeSpacing;
+			addPipes(newPos.x);
+		}
+	}
+
+	function addPipes(posX) {
+		var holeY = getRandomHolePosition();
 		var bottom = new Sprite(context, null, "pipe-bottom", {x: posX, y: holeY});
-		bottom.setVisible(true);
-
-
-
 		var top = new Sprite(context, null, "pipe-top");
 		var topPos = {
 			x: posX,
-			y: holeY - 12 - top.getSize().y,
+			y: holeY - holeHeight - top.getSize().y,
 		}
 		top.setPosition(topPos);
+		bottom.setVisible(true);
 		top.setVisible(true);
+		pipes.push([top, bottom]);
 	}
 
+	function getRandomHolePosition() {
+		var range = [195, 420];
+		return Math.floor(Math.random() * (range[1]-range[0])) + range[0];
+	}
 	construct();
 }
 
